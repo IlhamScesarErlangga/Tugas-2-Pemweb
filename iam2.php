5b5db9050459fd519db2cf3d7a0ff134cd8a587e
IlhamScesarErlangga <104293906+IlhamScesarErlangga@users.noreply.github.com>
2022-10-06 20:45:51 +0700

    Add files via upload

---
+++ iam2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Informasi Produk</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <table border="1">
+        <thead>
+            <th>Informasi Produk</th>
+        </thead>
+        <tr>
+            <td>Nama</td>
+            <td>:</td>
+            <td>Snack / Ciki</td>
+        </tr>
+        <tr>
+            <td>Jumlah</td>
+            <td>:</td>
+            <td>100</td>
+        </tr>
+        <tr>
+            <td>Jenis</td>
+            <td>:</td>
+            <td>Makanan / Minuman</td>
+        </tr>
+    </table>
+</body>
+</html>
